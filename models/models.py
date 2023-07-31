08c66185b44d7d58447df49652781498cc848044
guangasig <guangasigtsm@gmail.com>
2023-07-31 08:00:17 -0500

    Add: Configuraciones iniciales

---
+++ models/models.py
@@ -0,0 +1,15 @@
+# modelos de open AI
+
+class OpenAIModels:
+    # Modelo de lenguaje base de OpenAI (GPT-3.5)
+    DAVINCI = "text-davinci-002"
+    # Modelo de lenguaje Turbo de OpenAI (GPT-3.5 Turbo)
+    TURBO = "text-turbo-002"
+    # Modelo de lenguaje basado en GPT-4:
+    GPT4 = "text-gpt4.0"
+    # Modelo de lenguaje multilingüe
+    MULTILINGUAL = "text-gpt4.0-turbo-multilingual"
+    # Modelo de lenguaje especializado en código de programación
+    CODE = "code-gpt4.0-turbo"
+    # Modelo de lenguaje especializado en asistencia médica
+    MEDICAL = "text-medical-gpt4.0-turbo"
