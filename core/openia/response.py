7f0ab0edf7d6badf7067090131d06a7754a85cde
guangasig <guangasigtsm@gmail.com>
2023-08-08 17:30:02 -0500

    Update: reponse openia

--- core/openia/response.py
+++ core/openia/response.py
@@ -6,18 +6,20 @@ from models.OpenAIModels import OpenAIModels
 
 
 class Openia:
-    def __init__(self, input_text):
+    def __init__(self):
         self.input_text = None
         self.api_key = config('OPENAI_TOKEN', default='DEFAULT')
 
     def response(self, input_text):
+        print('input_text=', input_text)
+        print('self.input_text=', self.input_text)
         try:
             openai.api_key = self.api_key
             response = openai.ChatCompletion.create(
                 model=OpenAIModels.TURBO,
                 messages=[
                     {"role": "system", "content": "You are a helpful assistant."},
-                    {"role": "user", "content": input_text}
+                    {"role": "user", "content": self.input_text}
                 ]
             )
             return response['choices'][0]['message']['content']
